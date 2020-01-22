040e2f320dc69c8967d8ee4e9926875327b9d49a
hugo-cfc <53067210+hugo-cfc@users.noreply.github.com>
2020-01-22 10:59:24 -0300

    Update challenge-05.js

--- challenge-05/challenge-05.js
+++ challenge-05/challenge-05.js
@@ -2,27 +2,27 @@
 Crie uma variável qualquer, que receba um array com alguns valores aleatórios
 - ao menos 5 - (fica por sua conta os valores do array).
 */
-// let myArray = ['Hugo', [3, 4], {nome: 'Hugo', idade: 22}, 12, function myFunction() {return `Olá`}]
+let myArray = ['Hugo', [3, 4], {nome: 'Hugo', idade: 22}, 12, function myFunction() {return `Olá`}]
+/*   /C
 
-/*
 Crie uma função que receba um array como parâmetro, e retorne esse array.
 */
-// function myFunction2(arg) {
+ function myFunction2(arg) {
     return arg
 }
+/*    /C
 
-/*
 Imprima o segundo índice do array retornado pela função criada acima.
 */
-// myFunction2(myArray[1]) // [3, 4]
+myFunction2(myArray[1]) // [3, 4]
+/*   /C
 
-/*
 Crie uma função que receba dois parâmetros: o primeiro, um array de valores; e o
 segundo, um número. A função deve retornar o valor de um índice do array que foi passado
 no primeiro parâmetro. O índice usado para retornar o valor, deve ser o número passado no
 segundo parâmetro.
 */
-// function myFunction2(arg1, arg2) {
+function myFunction2(arg1, arg2) {
     if (arg2 === undefined){
         return arg1
     }
@@ -33,14 +33,18 @@ segundo parâmetro.
 /*
 Declare uma variável que recebe um array com 5 valores, de tipos diferentes.
 */
-// let myArray = ['Hugo', [3, 4], {nome: 'Hugo', idade: 22}, 12, function myFunction() {return `Olá`}]
+let myArray = ['Hugo', [3, 4], {nome: 'Hugo', idade: 22}, 12, function myFunction() {return `Olá`}]
 
 
 /*
 Invoque a função criada acima, fazendo-a retornar todos os valores do último
 array criado.
 */
-// myFunction2(myArray)
+myFunction2(myArray, 0)
+myFunction2(myArray, 1)
+myFunction2(myArray, 2)
+myFunction2(myArray, 3)
+myFunction2(myArray, 4)
 
 /*
 Crie uma função chamada `book`, que recebe um parâmetro, que será o nome do
@@ -56,56 +60,65 @@ propriedades:
 - Se o parâmetro não for passado, a função deve retornar o objeto com todos
 os livros.
 */
-// function book(nome) {
-    let myObj = {
-        saberViver : {
+function book(nome) {
+    let obj = {
+        'Saber Viver' : {
             quantidadePaginas: 362,
             autor: 'Roberto',
             editora: 'Aurora'
         },
 
-        noites : {
+        'Noites' : {
             quantidadePaginas: 567,
             autor: 'Cristina',
             editora: 'Viva'
         },
 
-        moita : {
+        'Moita' : {
             quantidadePaginas: 456,
             autor: 'Marcos',
             editora: 'Jota'
         }
         }
-        return myObj = nome
+
+    --------------------------------------------
+        if (nome === undefined) {
+            return obj
+        }
+        return obj[nome]
+    --------------------------------------------
+        return !nome ? obj : obj[nome] 
+ 
     }
 
-    
-
-
-console.log(book(saberViver))
+   
+console.log(book('Saber Viver'))
 
 /*
 Usando a função criada acima, imprima o objeto com todos os livros.
 */
-// ?
+book()
 
 /*
 Ainda com a função acima, imprima a quantidade de páginas de um livro qualquer,
 usando a frase:
 "O livro [NOME_DO_LIVRO] tem [X] páginas!"
 */
-// ?
+let nome2 = 'Saber Viver'
+`O livro ${nome2} tem ${book('nome2').quantidadePaginas} páginas.`
+---------------------------------------------------------------------------
+`O livro Saber Viver tem ${book('saberViver').quantidadePaginas} páginas.`
 
 /*
 Ainda com a função acima, imprima o nome do autor de um livro qualquer, usando
 a frase:
 "O autor do livro [NOME_DO_LIVRO] é [AUTOR]."
 */
-// ?
+`O autor do livro Saber Viver é ${book('saberViver').autor}!`
 
 /*
 Ainda com a função acima, imprima o nome da editora de um livro qualquer, usando
 a frase:
 "O livro [NOME_DO_LIVRO] foi publicado pela editora [NOME_DA_EDITORA]."
 */
-// ?
+`O livro Saber Viver foi publicado pela editora ${book('saberViver').editora}.`
